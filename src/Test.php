27e2f69a451168e823e95c41f84da696b56d1be9
Vladimir Bevzenko <bv@infoservice.ru>
2021-01-05 20:59:21 +0300

    Composer bootstrap

---
+++ src/Test.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace Vbevdev\LaravelBitrix24Logger;
+
+class Test
+{
+    public static function test()
+    {
+        return "it's work";
+    }
+}
---
+++ tests/test.php
@@ -0,0 +1,7 @@
+<?php
+
+require_once __DIR__ . '/../vendor/autoload.php';
+
+use Vbevdev\LaravelBitrix24Logger\Test;
+
+echo Test::test();
